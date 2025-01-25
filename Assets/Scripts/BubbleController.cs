6010c9dcd725cc34d730d12cc353123cdb2ccf3c
Lucas Hom <lucasrylanhom@gmail.com>
2025-01-25 13:43:09 -0800

    Added Dirty Bubbles
    - added dirty bubbles
    - added downward Tetris motion

--- Assets/Scripts/BubbleController.cs
+++ Assets/Scripts/BubbleController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class BubbleController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     public static int numBubbles = 0;
 
@@ -14,9 +15,15 @@ public class BubbleController : MonoBehaviour
     private float movement = 0f;
     [SerializeField] public float bubbleSpeed;
     [SerializeField] private float accelerationRate;
+    private float tempYVelocity;
+    [SerializeField] private float forcedYVelocity;
+
+
 
     //StuckBubble
     public bool isStuck;
+    [SerializeField] Sprite dirtyBubbleSprite;
+    private List<Collision2D> connectedList = new List<Collision2D>();
 
     void Start()
     {
@@ -26,7 +33,22 @@ public class BubbleController : MonoBehaviour
 
     void Update()
     {
-        getMovement();
+        //Stop if attatched to object
+        if (!isStuck)
+        {
+            getMovement();
+        }
+
+
+        //Change later to not call all the time
+        foreach (var col in connectedList)
+        {
+            if (col.gameObject.CompareTag("DirtyBubble"))
+            {
+                convertToDirty();
+            }
+        }
+
     }
 
     private void getMovement()
@@ -42,7 +64,20 @@ public class BubbleController : MonoBehaviour
         else
         {
             currentHorizontalInput = Mathf.MoveTowards(currentHorizontalInput, 0, accelerationRate * Time.deltaTime);
+
         }
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            tempYVelocity = rb2d.velocity.y;
+            rb2d.velocity = new Vector2(movement, forcedYVelocity);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            rb2d.velocity = new Vector2(movement, tempYVelocity);
+        }
+
 
         movement = currentHorizontalInput * bubbleSpeed;
     }
@@ -50,43 +85,74 @@ public class BubbleController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        //Create fixed joint with sticky surface when collide
+
+        //Create fixed joint with sticky surface when collide with sticky surface
         if (col.gameObject.tag == "StickySurface")
         {
-            isStuck = true;
-            FixedJoint2D fixedJoint = gameObject.AddComponent<FixedJoint2D>();
-
-            fixedJoint.connectedBody = col.rigidbody;
-
-            fixedJoint.breakForce = 1000f; 
-            fixedJoint.breakTorque = 1000f; 
-
-            Debug.Log("FixedJoint2D created and attached to " + col.gameObject.name);
+            createFixedJoint(col);
         }
 
         //Create hinge with bubble when collide
+        if (col.gameObject.tag == "DirtySurface")
+        {
+            convertToDirty();
+            createFixedJoint(col);
+        }
+
+        //Create hinge with bubble when collide
+        if (col.gameObject.tag == "DirtyBubble")
+        {
+            convertToDirty();
+            createHingeJoint(col);
+        }
+
+        //Create hinge with bubble when collide with other healthy bubble
         if (col.gameObject.tag == "HealthyBubble")
         {
-            isStuck = true;
-            HingeJoint2D hingeJoint = gameObject.AddComponent<HingeJoint2D>();
-
-            hingeJoint.connectedBody = col.rigidbody;
-
-            hingeJoint.breakForce = 1000f;
-            hingeJoint.breakTorque = 1000f; 
-
-            Debug.Log("HingeJoint2D created and attached to " + col.gameObject.name);
+            createHingeJoint(col);
         }
+    }
+
+    private void createFixedJoint(Collision2D col)
+    {
+        connectedList.Add(col);
+        isStuck = true;
+        FixedJoint2D fixedJoint = gameObject.AddComponent<FixedJoint2D>();
+
+        fixedJoint.connectedBody = col.rigidbody;
+
+        fixedJoint.breakForce = 100000f;
+        fixedJoint.breakTorque = 100000f;
+
+        Debug.Log("FixedJoint2D created and attached to " + col.gameObject.name);
+    }
+
+    private void createHingeJoint(Collision2D col)
+    {
+        connectedList.Add(col);
+        isStuck = true;
+        HingeJoint2D hingeJoint = gameObject.AddComponent<HingeJoint2D>();
+
+        hingeJoint.connectedBody = col.rigidbody;
+
+        hingeJoint.breakForce = 1000f;
+        hingeJoint.breakTorque = 1000f;
+
+        //Debug.Log("HingeJoint2D created and attached to " + col.gameObject.name);
+    }
+
+    private void convertToDirty()
+    {
+        gameObject.tag = "DirtyBubble";
+        spriteRenderer.sprite = dirtyBubbleSprite;
     }
 
 
     private void FixedUpdate()
     {
-        //Move by collected movement and stop if attatched to object
-        if (!isStuck)
-        {
-            rb2d.velocity = new Vector2(movement, rb2d.velocity.y);
-        }
+        //Move by collected movement
+
+        rb2d.velocity = new Vector2(movement, rb2d.velocity.y);
 
     }
 
